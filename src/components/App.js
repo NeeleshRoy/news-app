fac5a36df100de30deb877ca583afa2f1eb2d694
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-08 20:00:28 +0530

    Project bootstrapped

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        News App
+      </div>
+    );
+  }
+}
+
+export default App;
